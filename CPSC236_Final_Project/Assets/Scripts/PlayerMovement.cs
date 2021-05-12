0d1b411354c66586ef526351b93e0e664dbe6b5c
Tarek <elhaj102@mail.chapman.edu>
2021-05-12 15:03:32 -0700

    added player movement and characer controller from the 2d platformer

---
+++ CPSC236_Final_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 25f;
+    public bool hasJumpPotion = false;
+    public bool hasSpeedPotion = false;
+    public int potionModAmount = 0;
+
+    public AudioClip jumpClip;
+
+    private float potionTimeMax = 10f;
+    private float potionTimeCur = 0f;
+    private float horizontalMove = 0f;
+    private bool jumpFlag = false;
+    private bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (jumpFlag)
+        {
+            animator.SetBool("IsJumping", true);
+            jumpFlag = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (animator.GetBool("IsJumping") == false)
+            {
+                AudioSource.PlayClipAtPoint(jumpClip, transform.position);
+                animator.SetBool("IsJumping", true);
+                jump = true;
+            }
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+        jump = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (hasJumpPotion && potionTimeCur < potionTimeMax)
+        {
+            controller.m_JumpForceMod = potionModAmount;
+            potionTimeCur += Time.fixedDeltaTime;
+        }
+        else
+        {
+            potionTimeCur = 0f;
+            controller.m_JumpForceMod = 0;
+            hasJumpPotion = false;
+        }
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        if (jump)
+        {
+            jumpFlag = true;
+        }
+    }
+}
