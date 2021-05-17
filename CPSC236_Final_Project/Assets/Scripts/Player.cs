3c6022cb40b7295d1620cebaf6718cdb57ecbd12
Tarek <elhaj102@mail.chapman.edu>
2021-05-16 21:25:06 -0700

    added healthbar to main character

--- CPSC236_Final_Project/Assets/Scripts/Player.cs
+++ CPSC236_Final_Project/Assets/Scripts/Player.cs
@@ -5,92 +5,99 @@ using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed = 6f;
-    public Animator animator;
+    //public Rigidbody2D rb;
+    //public float moveSpeed = 6f;
+    //public Animator animator;
 
-    public Vector2 targetPosition; // cursor clicked position
-    public Vector2 relativePosition;  // relativePosition - character relative position
-    //private Vector2 movement;  // movement - var that stores movement position after a click
+    //public Vector2 targetPosition; // cursor clicked position
+    //public Vector2 relativePosition;  // relativePosition - character relative position
+    ////private Vector2 movement;  // movement - var that stores movement position after a click
 
-    private float firstClickTime, timeBetweenClicks;
-    private bool coroutineAllowed;
-    private int clickCounter;
+    //private float firstClickTime, timeBetweenClicks;
+    //private bool coroutineAllowed;
+    //private int clickCounter;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
 
     private void Start()
     {
-        firstClickTime = 0f;
-        timeBetweenClicks = 0.2f;
-        clickCounter = 0;
-        coroutineAllowed = true;
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        //firstClickTime = 0f;
+        //timeBetweenClicks = 0.2f;
+        //clickCounter = 0;
+        //coroutineAllowed = true;
     }
 
     void Update()
     {
-        animator.SetFloat("Speed", (Mathf.Abs(targetPosition.x - gameObject.transform.position.x)));
-
-        // set the mouse position
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            clickCounter++;
-            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            TakeDamage(20);
         }
-        if (clickCounter == 1 && coroutineAllowed)
-        {
-            moveSpeed = 6;
-            firstClickTime = Time.time;
-            StartCoroutine(DoubleClickDetection());
-        }
+        //animator.SetFloat("Speed", (Mathf.Abs(targetPosition.x - gameObject.transform.position.x)));
+
+        //// set the mouse position
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    clickCounter++;
+        //    targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //}
+        //if (clickCounter == 1 && coroutineAllowed)
+        //{
+        //    moveSpeed = 6;
+        //    firstClickTime = Time.time;
+        //    StartCoroutine(DoubleClickDetection());
+        //}
 
 
-        // relative poistion of the target based upon the current position
-        relativePosition = new Vector2(
-            targetPosition.x - gameObject.transform.position.x,
-            targetPosition.y - gameObject.transform.position.y);
+        //// relative poistion of the target based upon the current position
+        //relativePosition = new Vector2(
+        //    targetPosition.x - gameObject.transform.position.x,
+        //    targetPosition.y - gameObject.transform.position.y);
     }
 
-    private IEnumerator DoubleClickDetection()
+    void TakeDamage(int damage)
     {
-        coroutineAllowed = false;
-        while (Time.time < firstClickTime + timeBetweenClicks)
-        {
-            if (clickCounter == 2)
-            {
-                Debug.Log("Double Click");
-                moveSpeed = 12;
-                yield return new WaitForSeconds(1.5f); // give player 2x speed for 1.5 seconds
-            }
-            else
-            {
-                yield return new WaitForEndOfFrame();
-            }
+        currentHealth -= damage;
 
-        }
-        clickCounter = 0;
-        firstClickTime = 0f;
-        coroutineAllowed = true;
+        healthBar.SetHealth(currentHealth);
     }
 
-
-    void FixedUpdate()
-    {
-        Move();
-    }
-
-    void Move()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
-    }
-
-    //private bool IsMoving()
+    //private IEnumerator DoubleClickDetection()
     //{
-    //    if ((Mathf.Abs(targetPosition.x - gameObject.transform.position.x) > 0.01) ^ (Mathf.Abs(targetPosition.y - gameObject.transform.position.y) > 0.01))
+    //    coroutineAllowed = false;
+    //    while (Time.time < firstClickTime + timeBetweenClicks)
     //    {
-    //        Debug.Log("Moving!");
-    //        return true;
+    //        if (clickCounter == 2)
+    //        {
+    //            Debug.Log("Double Click");
+    //            moveSpeed = 12;
+    //            yield return new WaitForSeconds(1.5f); // give player 2x speed for 1.5 seconds
+    //        }
+    //        else
+    //        {
+    //            yield return new WaitForEndOfFrame();
+    //        }
+
     //    }
-    //    else
-    //        return false;
+    //    clickCounter = 0;
+    //    firstClickTime = 0f;
+    //    coroutineAllowed = true;
+    //}
+
+
+    //void FixedUpdate()
+    //{
+    //    Move();
+    //}
+
+    //void Move()
+    //{
+    //    transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
     //}
 
 }
