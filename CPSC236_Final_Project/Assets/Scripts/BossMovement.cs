45aa554ccf26b6c4ba7956166b7428b00be13368
Tarek <elhaj102@mail.chapman.edu>
2021-05-21 01:38:01 -0700

    scenes 1 and 3 are working need to fix scene 2 movement

---
+++ CPSC236_Final_Project/Assets/Scripts/BossMovement.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // vars for spawning enemies
+    public float timerMin = 5f;
+    public float timerMax = 25f;
+
+    // player ~ user
+    public Level3_Player player;
+    public Animator animator;
+
+    // vars for enemy movement
+    private Vector2 relativePosition;
+    private float moveSpeed = 1.6f;
+    private Vector2 enemyMovement;
+    private float enemyDetectionRadius = 1.5f;
+    // vars for rand enemy movement
+    private Vector2 movementDirection;
+    private Vector2 movementPerSecond;
+    private float latestDirectionChangeTime;
+    private readonly float directionChangeTime = 3f;
+
+    public bool canAttackPlayer = false;
+    bool shouldFlip = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // calculations for enemy movement
+        latestDirectionChangeTime = 0f;
+        calcuateNewMovementVector();
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(moveSpeed, moveSpeed);
+        canAttackPlayer = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this == null || player == null)
+        {
+            return;
+        }
+
+        if (player != null)
+        {
+            // relative poistion of the target (player) based upon the current position
+            relativePosition = new Vector2(
+                player.GetComponent<Rigidbody2D>().position.x - gameObject.transform.position.x,
+                player.GetComponent<Rigidbody2D>().position.y - gameObject.transform.position.y);
+        }
+
+        if (relativePosition.x < enemyDetectionRadius && relativePosition.y < enemyDetectionRadius)
+        {
+            canAttackPlayer = true;
+        }
+        else
+        {
+            canAttackPlayer = false;
+            //if the changeTime was reached, calculate a new movement vector
+            if (Time.time - latestDirectionChangeTime > directionChangeTime)
+            {
+                latestDirectionChangeTime = Time.time;
+                calcuateNewMovementVector();
+            }
+        }
+
+        if (canAttackPlayer)
+        {
+            if ((player.GetComponent<Rigidbody2D>().position.x - gameObject.transform.position.x) > 0 && this.transform.localScale.x > 0)
+            {
+                shouldFlip = true;
+            }
+            else if ((player.GetComponent<Rigidbody2D>().position.x - gameObject.transform.position.x) < 0 && this.transform.localScale.x < 0)
+            {
+                shouldFlip = true;
+            }
+            else
+            {
+                shouldFlip = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (this == null)
+        {
+            return;
+        }
+
+        if (shouldFlip)
+        {
+            Flip();
+        }
+        if (player != null && canAttackPlayer)
+        {
+            MoveTowardsPlayer();
+        }
+    }
+
+
+    void MoveTowardsPlayer()
+    {
+        //animator.SetTrigger("Monster-Boss-Walk");
+        if (moveSpeed * Time.deltaTime >= Mathf.Abs(relativePosition.x))
+        {
+            enemyMovement.x = relativePosition.x;
+        }
+        else
+        {
+            enemyMovement.x = moveSpeed * Mathf.Sign(relativePosition.x);
+        }
+
+        rb.velocity = enemyMovement;
+    }
+
+    void calcuateNewMovementVector()
+    {
+        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), 0).normalized;
+        movementPerSecond = movementDirection * moveSpeed;
+    }
+
+    public void Flip()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+}
---
+++ CPSC236_Final_Project/Assets/Scripts/Level3_PlayerMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3_PlayerMovement : MonoBehaviour
+{
+    
+    public Level3_CharacterController2D controller;
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public int attackDamage = 100;
+    public float attackRange = 2.5f;
+
+    public float runSpeed = 25f;
+    public bool hasJumpPotion = false;
+    public bool hasSpeedPotion = false;
+    public int potionModAmount = 0;
+
+    public AudioClip jumpClip;
+
+    private float horizontalMove = 0f;
+    private bool jumpFlag = false;
+    private bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this == null)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+
+        if (jumpFlag)
+        {
+            jumpFlag = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+                jump = true;
+        }
+    }
+
+    void Attack()
+    {
+        // play attack animation
+        animator.SetTrigger("AttackTrigger");
+
+        // detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        if(hitEnemies == null)
+        {
+            return;
+        }
+
+        // damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Level3_Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    public void OnLanding()
+    {
+        jump = false;
+    }
+
+    void FixedUpdate()
+    {
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        if (jump)
+        {
+            jumpFlag = true;
+        }
+    }
+
+}
