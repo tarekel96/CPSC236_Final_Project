794396072def9e99be2bfe35f84b49d38e0a39c5
ninavaldez <78568283+ninavaldez@users.noreply.github.com>
2021-05-20 20:49:34 -0700

    Updating my branch

---
+++ CPSC236_Final_Project/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,50 @@
+﻿// Tarek El Hajjaoui, Nina Valdez, Joshua Wisdom
+// CPSC 236-03
+// elhaj102@mail.chapman.edu, divaldez@chapman.edu, jowisdom@chapman.edu
+// Final Project: Untitled Platformer
+// This is our own work, we did not cheat on this assignment
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is for when player attacks enemy, the enemy will loss health until it dies
+/// </summary>
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        Debug.Log("Enemy Hit!");
+        animator.SetTrigger("hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy Died!");
+        animator.SetBool("isDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
